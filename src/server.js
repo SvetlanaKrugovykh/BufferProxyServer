de1c0dc99371176aa9ad01b1592da700690672a4
SvetlanaKrugovykh <Svetlana.Krugovykh@gmail.com>
2024-02-01 20:57:59 +0100

    first fix

--- src/server.js
+++ src/server.js
@@ -1,14 +1,21 @@
 require('dotenv').config()
-const fastify = require('fastify')()
+const Fastify = require('fastify')
 const screenshotRoute = require('./routes/screenshotRoute')
-
-fastify.register(screenshotRoute)
 const PORT = process.env.PORT || 8982
+const HOST = process.env.HOST || '127.0.0.1'
 
-fastify.listen(PORT, (err, address) => {
+const app = Fastify({
+  trustProxy: true
+})
+
+app.register(screenshotRoute)
+
+app.listen({ port: process.env.PORT || 8080, host: HOST }, (err, address) => {
   if (err) {
-    console.error(err)
+    app.log.error(err)
     process.exit(1)
   }
-  console.log(`Server listening on ${address}`)
+
+  console.log(`${new Date()}:[API] Service listening on ${address}`)
 })
+
